e5d40201e1b327fdbe2da853988817614e581140
farqlia <juliafarganus@gmail.com>
2022-10-23 17:02:11 +0200

    cw 1 implementation cd

--- .vscode/lists/list2/ex3.ml
+++ .vscode/lists/list2/ex3.ml
@@ -1,12 +1,10 @@
-let root3 a = 
-  let precision = Float.pow 10. (-15.) in 
-    let rec root3Inner (a, x) = 
-      if Float.abs(Float.pow x 3. -. a) < precision *. Float.abs(a)
-        then x 
-    else root3Inner(a, x +. (a /. (x *. x) -. x) /. 3.)
-  in let initX = if a > 1. then a /. 3. else a
-  in root3Inner(a, initX);;
+let root3 a =  
+  let rec root3Inner a x = 
+    if Float.abs(Float.pow x 3. -. a) < Float.pow 10. (-15.) *. 
+      Float.abs(a) then x 
+  else root3Inner a (x +. (a /. (x *. x) -. x) /. 3.)
+  in root3Inner a (if a > 1. then a /. 3. else a);;
 
 root3 8.;;
-root3 27.;;
+root3 (-27.);;
 root3 100.;;
