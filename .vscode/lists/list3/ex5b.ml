db462c97a1b31ed24f0df503e8e073143c106d44
farqlia <juliafarganus@gmail.com>
2022-11-06 23:14:46 +0100

    list3

--- .vscode/lists/list3/ex5b.ml
+++ .vscode/lists/list3/ex5b.ml
@@ -1,18 +1,25 @@
-let mergesort pred xs = 
+let mergesort smaller_equal xs = 
   let rec split_to_list = function 
   | h::t -> [h]::split_to_list t 
   | [] -> []
 in let rec merge (xs1, xs2) = 
   match (xs1, xs2) with 
-  | (h1::t1, h2::t2) when pred h1 h2 ->
+  | (h1::t1, h2::t2) when smaller_equal h1 h2 ->
      h1::merge(t1, xs2)
   | (h1::t1, h2::t2) -> h2::merge(xs1, t2)
-  | ([], _) -> xs2 
   | (_, []) -> xs1
+  | ([], _) -> xs2 
+
   in let rec mergeinner xss = 
     match xss with 
     | h1::h2::t -> merge(h1, h2)::mergeinner t
-    | h -> h 
-  in mergeinner (split_to_list xs);;
+    | h -> h
+  in let rec loop xss = 
+    match xss with 
+    | [h] -> h
+    | _ -> loop (mergeinner xss)
+  in loop (split_to_list xs) ;; 
+  
+mergesort (<=) [13; 5; 10; 8; 6; 22; 11; 3; 12; 20; 7; 9; 14; 17; 19; 1; 2; 18];;
 
-mergesort (<) [13; 5; 10; 8; 6; 22; 11; 3; 12; 20; 7; 9; 14; 17; 19; 1; 2; 18];;
+mergesort (fun (x1, y1) (x2, y2) -> (x1 <= x2)) [(1, 0); (3, 0); (2, 0); (2, 1); (3, 1); (1, 1); (2, 2); (1, 2)];;
