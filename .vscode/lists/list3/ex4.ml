66cbc06e2c089ef08e6468d0a40e0b061b913879
farqlia <juliafarganus@gmail.com>
2022-11-07 17:52:27 +0100

    spac

--- .vscode/lists/list3/ex4.ml
+++ .vscode/lists/list3/ex4.ml
@@ -6,6 +6,7 @@ let rec quicksort = function
         in quicksort small @ quicksort large;; 
 
 quicksort([5; 5]);;
+quicksort([5; 4; 3; 2; 1; 5]);;
 quicksort([3; 3; 2; 4; 5; 3; 2; 5; 4]);;
 
 let rec quicksort' = function 
@@ -21,10 +22,9 @@ quicksort'([3; 3; 2; 4; 5; 3; 2; 5; 4]);;
 let rec quicksort'' = function 
 | [] -> []
 | [x] -> [x]
-| xs -> let small = List.filter (fun y -> y < List.hd xs) xs 
-        and large = List.filter (fun y -> y > List.hd xs) xs 
-        and equal = List.filter (fun y -> y = List.hd xs) xs 
-        in quicksort'' small @ equal @ quicksort'' large;; 
+| h::t -> let small = List.filter (fun y -> y < h) t
+        and large = List.filter (fun y -> y >= h) t
+        in quicksort'' small @ [h] @ quicksort'' large;; 
 
 quicksort''([5; 5; 5; 5]);;
 quicksort''([13; 5; 10; 8; 6; 22; 11; 3; 12; 20; 7; 9; 14; 17; 19; 1; 2; 18]);;
@@ -34,8 +34,7 @@ let rec quicksort4 = function
 | [] -> []
 | x::xs -> let small = List.filter (fun y -> y < x) xs 
           and large = List.filter (fun y -> y > x) xs 
-          and equal = List.filter (fun y -> y = x) xs 
-          in quicksort4 small @ equal @ (x::quicksort4 large);;
+          in quicksort4 small @ [x] @ (x::quicksort4 large);;
 
 quicksort4([13; 5; 10; 8; 6; 22; 11; 3; 12; 20; 7; 9; 14; 17; 19; 1; 2; 18]);;
 quicksort4([3; 3; 2; 4; 5; 3; 2; 5; 4]);;
--- .vscode/lists/list3/ex5b.ml
+++ .vscode/lists/list3/ex5b.ml
@@ -1,13 +1,13 @@
-let mergesort smaller_equal xs = 
+let mergesort pred xs = 
   let rec split_to_lists = function 
   | h::t -> [h]::split_to_lists t 
   | [] -> []
 in let rec merge (xs1, xs2) = 
   match (xs1, xs2) with 
-  | (h1::t1, h2::t2) when smaller_equal h1 h2 ->
-     h1::merge(t1, xs2)
+  | (h1::t1, h2::t2) when pred h2 h1 ->
+     h2::merge(xs1, t2)
      (*h1::t1, otherwise it'll match an empty list in place of xs1*)
-  | (h1::t1, h2::t2) -> h2::merge(xs1, t2)
+  | (h1::t1, h2::t2) -> h1::merge(t1, xs2)
   | (_, []) -> xs1
   | ([], _) -> xs2 
 
@@ -21,8 +21,8 @@ in let rec merge (xs1, xs2) =
     | _ -> loop (merge_two xss)
   in loop (split_to_lists xs) ;; 
   
-mergesort (<=) [13; 5; 10; 8; 6; 22; 11; 3; 12; 
+mergesort (<) [13; 5; 10; 8; 6; 22; 11; 3; 12; 
 20; 7; 9; 14; 17; 19; 1; 2; 18];;
 
-mergesort (fun (x1, y1) (x2, y2) -> (x1 <= x2)) [(1, 0); (3, 0); 
+mergesort (fun (x1, y1) (x2, y2) -> (x1 < x2)) [(1, 0); (3, 0); 
 (2, 0); (2, 1); (3, 1); (1, 1); (2, 2); (1, 2)];;
