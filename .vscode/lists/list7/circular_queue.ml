c5ec95265b314d2b67779639c74d88aee225724e
farqlia <juliafarganus@gmail.com>
2022-12-05 07:14:30 +0000

    queues

--- .vscode/lists/list7/circular_queue.ml
+++ .vscode/lists/list7/circular_queue.ml
@@ -18,19 +18,20 @@ struct
   exception Empty of string
   exception Full of string
 
-  let succ i q = succ(i) mod (Array.length q.arr)
+  let next i q = succ(i) mod (Array.length q.arr)
   let isEmpty q = q.r = q.f
-  let isFull q = succ (q.r) q = q.f
+  let isFull q = next (q.r) q = q.f
   let empty size = {f=0; r=0; arr=Array.make (size + 1) None}
   let enqueue (e, q) = 
   if isFull q then failwith "full queue"
   else q.arr.(q.r) <- Some(e);
-  q.r <- succ (q.r) q;;
-  let dequeue q = if not (isEmpty q) then q.f <- succ (q.f) q
+  q.r <- next (q.r) q;;
+  let dequeue q = if not (isEmpty q) then q.f <- next (q.f) q
 
-  let first q = if isEmpty q then raise (Empty "empty queue")
-                else let Some(e) = q.arr.(q.f) in e
-
+  let first q = if (isEmpty q) then raise (Empty "empty queue")
+                else match q.arr.(q.f) with 
+                | Some(e) -> e
+                | None -> failwith "Implementation Error"
 end;;
 
 let (<|) q e = QueueMut.enqueue (e, q); q;;
@@ -49,3 +50,4 @@ QueueMut.dequeue q;;
 QueueMut.isEmpty q;;
 QueueMut.isFull q;;
 QueueMut.first q;;
+
