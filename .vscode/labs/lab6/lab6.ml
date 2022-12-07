b317f596d13503fa0e225ab565753eb8f9528977
farqlia <juliafarganus@gmail.com>
2022-12-07 18:14:40 +0000

    dd

--- .vscode/labs/lab6/lab6.ml
+++ .vscode/labs/lab6/lab6.ml
@@ -1,21 +1,27 @@
-let copy source destination = 
-  for i = 0 to min(Array.length destination) (Array.length source) - 1 do 
-    destination.(i) <- source.(i)
-  done;;
-
 let pascal n = 
+  if n < 1 then failwith ("Illegal Argument = "^(string_of_int n))
+  else 
   let prev_row = Array.make (2 * n + 1) 1; 
   and curr_row = Array.make (2 * n + 1) 1; 
-  and r = ref 2 in 
+  and r = ref 2 and upper = ref 2 in 
+  
   while !r <= n do 
-    for i = 2 to 2 * !r - 2 do 
+    upper := 2 * !r - 2;
+    for i = 2 to !upper do 
       curr_row.(i) <- prev_row.(i - 2) + prev_row.(i - 1) + 
                       prev_row.(i)
     done;
-    r := succ !r; copy curr_row prev_row
+    
+    for i = 0 to !upper do
+      prev_row.(i) <- curr_row.(i)
+    done;
+
+    r := succ !r; 
+
   done;
   curr_row;; 
   
+pascal 0;;
 pascal 1;;
 pascal 2;;
 pascal 3;;
